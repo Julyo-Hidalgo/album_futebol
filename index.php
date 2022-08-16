c9d307507893a9ce055c4c98048ebbb3b313d403
Julyo <julyohidalgo0@gmail.com>
2022-08-16 15:56:24 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/estilo.css">
+        <title>Álbum de figurinhas</title>
+    </head>
+
+    <body>
+        <div class="interface">
+            <div>
+                <h1 class="titulo">Álbum de figurinhas</h1>
+            </div>
+        </div>
+    </body>
+</html>
